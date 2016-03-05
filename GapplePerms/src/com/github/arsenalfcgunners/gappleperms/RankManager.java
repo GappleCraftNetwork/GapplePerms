193eb831601aec96ce0b75a74202720e54da154e
arsenalfcgunners <arsenalfcgunnersplaysmc@gmail.com>
2016-03-05 13:41:09 -0800

    Minor bug fixes with permissions.

--- GapplePerms/src/com/github/arsenalfcgunners/gappleperms/RankManager.java
+++ GapplePerms/src/com/github/arsenalfcgunners/gappleperms/RankManager.java
@@ -37,17 +37,16 @@ public class RankManager {
 	}
 
 	public void addRanks() {
-		ranks.add(new Rank("Owner", ChatColor.DARK_RED+""+ChatColor.BOLD, 10, false));
-		ranks.add(new Rank("Dev",ChatColor.DARK_PURPLE+""+ChatColor.BOLD, 9, false));
-		ranks.add(new Rank("Admin", ChatColor.DARK_RED+""+ChatColor.BOLD, 8, false));
-		ranks.add(new Rank("SrMod", ChatColor.DARK_RED + "", 7, false));
-		ranks.add(new Rank("Mod", ChatColor.RED + "", 6, false));
-		ranks.add(new Rank("VIP", ChatColor.GOLD+"", 5, true));
-		ranks.add(new Rank("Builder", ChatColor.BLUE + "", 4, true));
-		ranks.add(new Rank("Diamond", ChatColor.DARK_AQUA + "", 3, true));
-		ranks.add(new Rank("Emerald", ChatColor.GREEN + "", 2, true));
-		ranks.add(new Rank("Amethyst", ChatColor.LIGHT_PURPLE + "", 1, true));
-		ranks.add(new Rank("Default", ChatColor.YELLOW + "", 0, false));
+		ranks.add(new Rank("Owner", ChatColor.DARK_RED, 9, false));
+		ranks.add(new Rank("Dev",ChatColor.DARK_PURPLE, 8, false));
+		ranks.add(new Rank("Admin", ChatColor.DARK_RED, 7, false));
+		ranks.add(new Rank("SrMod", ChatColor.RED, 6, false));
+		ranks.add(new Rank("Mod", ChatColor.GOLD, 5, false));
+		ranks.add(new Rank("VIP", ChatColor.AQUA, 4, true));
+		ranks.add(new Rank("Diamond", ChatColor.DARK_AQUA, 3, true));
+		ranks.add(new Rank("Emerald", ChatColor.GREEN, 2, true));
+		ranks.add(new Rank("Amethyst", ChatColor.LIGHT_PURPLE, 1, true));
+		ranks.add(new Rank("Default", ChatColor.YELLOW, 0, false));
 	}
 
 	public ArrayList<Rank> getRankList() {
